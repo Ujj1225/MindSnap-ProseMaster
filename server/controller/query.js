831a2748e1b6d1cb12e61bbe7075d82e36f0dae6
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-12-10 04:13:21 +0545

    Handling for concurrent requests using promise.all

--- server/controller/query.js
+++ server/controller/query.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 // import modules from OpenAI library
 const OpenAI = require("openai");
 
-
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -16,18 +15,37 @@ const query = async (req, res) => {
     if (prompt == null) {
       throw new Error("No prompt was provided!");
     }
+    const summarize_promt = `${prompt} \n Summarize the text in layman's terms with example`;
+    const keyword_promt = `${prompt} \n From the text extract the most important keywords and provide meaning to them`;
+    const mnemonics_promt = `${prompt} \n Provide mnemonics or make a story out of this text so that it can be easily remembered. Remember to keep it simple and in a way that it can easily be remembered by everyone`;
 
-    const response = await openai.completions.create({
-      model: "text-davinci-003",
-      prompt,
-    });
+    // Taking multiple requests in parallel
+    const [summarizeResponse, keywordResponse, mnemonicsResponse] =
+      await Promise.all([
+        openai.create({
+          model: "text-davinci-003",
+          prompt: summarize_prompt,
+        }),
+        openai.create({
+          model: "text-davinci-003",
+          prompt: keyword_prompt,
+        }),
+        openai.create({
+          model: "text-davinci-003",
+          prompt: mnemonics_prompt,
+        }),
+      ]);
 
-    const completion = response.choices[0].text;
+    const summarizeCompletion = summarizeResponse.choices[0].text;
+    const keywordCompletion = keywordResponse.choices[0].text;
+    const mnemonicsCompletion = mnemonicsResponse.choices[0].text;
 
-    // return the result
+    // return the results
     return res.status(200).json({
       success: true,
-      message: completion,
+      summarized_text: summarizeCompletion,
+      keywords: keywordCompletion,
+      howToRemember: mnemonicsCompletion,
     });
   } catch (error) {
     console.log(error.message);
